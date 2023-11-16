2c6bf92e2f97c3537eb125533ccc5a4d9d639843
clock <1162411462@qq.com>
2023-11-16 15:49:32 +0800

    简单的demo

---
+++ test/build/CMakeFiles/demo01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo01.dir/test1.cpp.o"
+  "demo01"
+  "demo01.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
